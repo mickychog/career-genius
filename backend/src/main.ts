22d39369fa881b0198c3b682ce3feea9829aca08
Miguel <mickychog@gmail.com>
2025-10-26 19:42:55 -0400

    feat: Enable CORS and connect services

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   //CORS habilitado para el frontend
   app.enableCors({
-    origin: 'https://career-genius.vercel.app/', 
+    origin: 'https://career-genius.vercel.app',
   });
   await app.listen(process.env.PORT || 3000);
 }
