ccd3e597111f7de5fa5ff1a1a96e4763624f80a3
Miguel <mickychog@gmail.com>
2025-10-26 19:03:22 -0400

    feat: actualization Dockerfile

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();cd
+  
+  //CORS habilitado para el frontend
+  app.enableCors({
+    origin: 'https://career-genius.vercel.app/', 
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// ¡La URL de tu backend en Render!
+const API_URL = "https://career-genius-backend.onrender.com";
 
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch(API_URL) 
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch(() => setMessage("Error fetching data"));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Message from Backend: <strong>{message}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
