d65fd9b7ccfc1a68b70fbafe4757638004e61730
Miguel <mickychog@gmail.com>
2025-10-26 20:06:39 -0400

    Feat: Fix CORS domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //CORS habilitado para el frontend
   app.enableCors({
-    origin: 'https://career-genius.vercel.app',
+    origin: ['https://career-genius.vercel.app'], 
+    credentials: true, 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
